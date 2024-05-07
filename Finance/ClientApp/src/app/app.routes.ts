5d2980fac3d453f826d0e6c42c574b033a110551
Tiago Gadonski Cordeiro <tiagogadonski@hotmail.com>
2024-05-07 14:24:09 -0300

    pages in angular

--- Finance/ClientApp/src/app/app.routes.ts
+++ Finance/ClientApp/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
-import { KanbanComponent } from './kanban/kanban.component'; // Ajuste o caminho conforme necessário
+import { KanbanComponent } from './kanban/kanban.component'; 
 import { DashboardTestComponent } from './dashboard-test/dashboard-test.component';
+import { BlogComponent } from './blog/blog.component';
+import { FinancesComponent } from './finances/finances.component';
+import { GoalsComponent } from './goals/goals.component';
+import { NotesComponent } from './notes/notes.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +15,26 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardTestComponent
-  }
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: 'finances',
+    component: FinancesComponent
+  },
+  {
+    path: 'goals',
+    component: GoalsComponent
+  },
+  {
+    path: 'notes',
+    component: NotesComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+
 ];
