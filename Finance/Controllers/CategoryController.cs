80ecbda96f402b9d0ef7183ce564f5d16b3c6fdb
Tiago Gadonski Cordeiro <tiagogadonski@hotmail.com>
2024-05-15 14:18:05 -0300

    task updates

---
+++ Finance/Controllers/CategoryController.cs
@@ -0,0 +1,64 @@
+﻿using Finance.Data;
+using Finance.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly FinanceContext _context;
+
+        public CategoryController(FinanceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Categories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        // POST: Categories/Create
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return Json(category);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // PUT: Categories/Edit/5
+        [HttpPut]
+        public async Task<IActionResult> Edit([FromBody] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(category);
+                await _context.SaveChangesAsync();
+                return Json(category);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // DELETE: Categories/Delete/5
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return Json(category);
+        }
+    }
+}
---
+++ Finance/Controllers/ColumnController.cs
@@ -0,0 +1,64 @@
+﻿using Finance.Data;
+using Finance.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance.Controllers
+{
+    public class ColumnController : Controller
+    {
+        private readonly FinanceContext _context;
+
+        public ColumnController(FinanceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Columns
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Columns.ToListAsync());
+        }
+
+        // POST: Columns/Create
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Column column)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(column);
+                await _context.SaveChangesAsync();
+                return Json(column);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // PUT: Columns/Edit/5
+        [HttpPut]
+        public async Task<IActionResult> Edit([FromBody] Column column)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(column);
+                await _context.SaveChangesAsync();
+                return Json(column);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // DELETE: Columns/Delete/5
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var column = await _context.Columns.FindAsync(id);
+            if (column == null)
+            {
+                return NotFound();
+            }
+
+            _context.Columns.Remove(column);
+            await _context.SaveChangesAsync();
+            return Json(column);
+        }
+    }
+}
