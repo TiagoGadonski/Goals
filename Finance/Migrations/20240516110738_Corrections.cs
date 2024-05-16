677644b2dcef98aa97d35db44fad18bbb7374814
Tiago Gadonski Cordeiro <tiagogadonski@hotmail.com>
2024-05-16 10:23:42 -0300

    home upgrades

---
+++ Finance/Migrations/20240516110738_Corrections.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Finance.Migrations
+{
+    /// <inheritdoc />
+    public partial class Corrections : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreationDate",
+                table: "Wish",
+                newName: "Amount");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "Wish",
+                newName: "CreationDate");
+        }
+    }
+}
---
+++ Finance/Migrations/20240516114949_DeleteWish.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Finance.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteWish : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Wish");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
